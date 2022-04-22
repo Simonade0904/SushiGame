39913024edb8c8b4b52cb3f7e8d7ae375dcfe214
QW <simonade@Simonade.attlocal.net>
2022-04-22 15:55:09 -0400

    Finalize game and add instructions

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,26 @@ export default function Home() {
       <main>
         <h1>The Great Sushi Go Simulator</h1>
         <Link href='https://www.gamewright.com/gamewright/pdfs/Rules/SushiGoTM-RULES.pdf'>
-          <a>PDF about Sushi Go's rules</a>
+          <a>PDF about Sushi Go's complete rules</a>
         </Link>
+        
+        <div className="guide-container">
+          <h3>Quick Guide</h3>
+          <ol>
+            <li>We will play a simplfied version of Sushi Go. There are four players total. You, and 3 other AI players.</li>
+            <li>Each player receives a hand of 8 cards. You take 1 card, and pass the hand to the person that's next to you. In this game, you press the "rotate" button.</li>
+            <li>Repeat until all the cards are taken.</li>
+          </ol>
+          <hr></hr>
+          <h3>Cards Overview</h3>
+          <ul>
+            <li>Nigiri: Each individual card is worth 2 points.</li>
+            <li>Tempura: Each pair of them is worth 5 points. 0 if you don't get a pair.</li>
+            <li>Sashimi: Each triplet of them is worth 10 points. 0 if you don't get enough to form a triplet.</li>
+            <li>Dumpling: 1 point for 1 dumpling, 3 points for 2, 6 points for 3, 10 points for 4, 15 points for 5 or more of them.</li>
+          </ul>
+        </div>
+
         <Link href='/game/start-game'>
           <button className="btn btn-primary" href='/game/start-game'>Start Game</button>
         </Link>
@@ -25,6 +43,13 @@ export default function Home() {
       <footer>
         <p>Created with &#128153; by Qingyi Wang</p>
       </footer>
+      <style jsx>{`
+      .guide-container{
+        border: 1px solid black;
+        border-radius: 0.5rem;
+        padding: 24px;
+      }
+    `}</style>
     </Layout>
   )
 }
